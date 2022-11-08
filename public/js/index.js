fa129835bc97ce90c68d8b27b4c224234dbfcbdb
Andy Ni <andyni058@gmail.com>
2022-11-08 14:26:34 -0500

    screenshots and fix shooting stars

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,5 @@
+import imgCollection from './pics.js';
+
 const nav = document.querySelector('.navbar');
 const sections = document.querySelectorAll('section');
 const navLinks = document.querySelectorAll('li');
@@ -47,3 +49,67 @@ const navScrollObeserver = new IntersectionObserver((entries, observer) => {
 sections.forEach((section) => {
   navScrollObeserver.observe(section);
 });
+
+//Image Modal
+const modal = document.querySelector('.modal-container');
+const modalPage = document.querySelector('.modal-page');
+const modalImg = document.querySelector('.modal-img');
+const modalDesc = document.querySelector('.modal-desc');
+const modalNext = document.querySelector('.modal-next');
+const modalPrev = document.querySelector('.modal-prev');
+const modalClose = document.querySelector('.modal-close');
+const screenshots = document.querySelectorAll('[data-screen]');
+
+let currpage = 0;
+let totalpage = 0;
+let arr;
+
+screenshots.forEach((screenshot) => {
+  screenshot.addEventListener('click', (e) => {
+    const type = e.target.dataset.screen;
+    if (type === 'anidisc') {
+      arr = imgCollection.anidisc;
+    } else if (type === 'gacha') {
+      arr = imgCollection.gacha
+    } else if (type === 'dailyten') {
+      arr = imgCollection.dailyten
+    } else if (type === 'pokedex') {
+      arr = imgCollection.pokedex
+    } else if (type === 'weather') {
+      arr = imgCollection.weather
+    }
+    currpage = 0;
+    totalpage = arr.length;
+    changeInner();
+    document.body.style.overflow = 'hidden'
+    modal.classList.add('active');
+  });
+});
+
+function changeInner() {
+  if(currpage >= totalpage){
+    currpage = 0
+  } else if (currpage < 0){
+    currpage = totalpage - 1
+  }
+  modalPage.innerText = `${currpage + 1}/${totalpage}`;
+  modalImg.src = arr[currpage].img;
+  modalDesc.innerText = arr[currpage].desc
+}
+
+function changePage(direction) {
+  if (direction === 'next') {
+    currpage = currpage + 1;
+    changeInner();
+  } else {
+    currpage = currpage - 1;
+    changeInner();
+  }
+}
+
+modalNext.addEventListener('click', () => changePage('next'));
+modalPrev.addEventListener('click', () => changePage('prev'));
+modalClose.addEventListener('click', () => {
+  modal.classList.remove('active');
+  document.body.style.overflow = "initial"
+})
---
+++ public/js/pics.js
@@ -0,0 +1,164 @@
+const anidisc = [
+  {
+    img: './public/imgs/anidisc/main.png',
+    desc: 'Home page',
+  },
+  {
+    img: './public/imgs/anidisc/top.png',
+    desc: 'Top Anime ',
+  },
+  {
+    img: './public/imgs/anidisc/filter.png',
+    desc: 'Search and filter anime',
+  },
+  {
+    img: './public/imgs/anidisc/post.png',
+    desc: 'Browse posts on specific anime',
+  },
+  {
+    img: './public/imgs/anidisc/posting.png',
+    desc: 'Make post on anime, description and img optional',
+  },
+  {
+    img: './public/imgs/anidisc/comment.png',
+    desc: 'Make or view comments under posts',
+  },
+  {
+    img: './public/imgs/anidisc/user.png',
+    desc: 'User profile',
+  },
+  {
+    img: './public/imgs/anidisc/anotheruser.png',
+    desc: 'View of other user profile',
+  },
+  {
+    img: './public/imgs/anidisc/mobile.png',
+    desc: 'Mobile view',
+  },
+];
+
+const dailyten = [
+  {
+    img: './public/imgs/dailyten/defaulthome.png',
+    desc: 'Home page before login',
+  },
+  {
+    img: './public/imgs/dailyten/home.png',
+    desc: 'Home page after login',
+  },
+  {
+    img: './public/imgs/dailyten/createform.png',
+    desc: 'Form to add building for landlords',
+  },
+  {
+    img: './public/imgs/dailyten/findbuilding.png',
+    desc: 'Page for tenants to find buildings to join',
+  },
+  {
+    img: './public/imgs/dailyten/announce.png',
+    desc: 'Announcement page, landlords(left), tenants(right)',
+  },
+  {
+    img: './public/imgs/dailyten/panels.png',
+    desc: 'Menu for each building, landlords(left), tenants(right)',
+  },
+  {
+    img: './public/imgs/dailyten/request.png',
+    desc: 'Landlord can manage tenants on this page',
+  },
+  {
+    img: './public/imgs/dailyten/repairlist.png',
+    desc: 'List of repair requests from tenants',
+  },
+  {
+    img: './public/imgs/dailyten/repair.png',
+    desc: 'Tenants and Landlords can converse about specific repairs',
+  },
+  {
+    img: './public/imgs/dailyten/setting.png',
+    desc: 'Settings to reset password and change layout of site',
+  },
+  {
+    img: './public/imgs/dailyten/mobile.png',
+    desc: 'Mobile view',
+  },
+];
+
+const gacha = [
+  {
+    img: './public/imgs/gacha/main.png',
+    desc: 'Main page',
+  },
+  {
+    img: './public/imgs/gacha/main_mobile.png',
+    desc: 'Main page mobile',
+  },
+  {
+    img: './public/imgs/gacha/summon.png',
+    desc: 'Summoning animation',
+  },
+  {
+    img: './public/imgs/gacha/result.png',
+    desc: 'Summon results page',
+  },
+  {
+    img: './public/imgs/gacha/result_mobile.png',
+    desc: 'Summon results page mobile',
+  },
+  {
+    img: './public/imgs/gacha/purchase.png',
+    desc: 'Purchase currency for summoning',
+  },
+  {
+    img: './public/imgs/gacha/history.png',
+    desc: 'History of past results',
+  },
+];
+
+const pokedex = [
+  {
+    img: './public/imgs/pokedex/main.png',
+    desc: 'Main page',
+  },
+  {
+    img: './public/imgs/pokedex/modal.png',
+    desc: 'Modal view',
+  },
+  {
+    img: './public/imgs/pokedex/modal_2.png',
+    desc: 'Modal evolution',
+  },
+  {
+    img: './public/imgs/pokedex/modal_multi.png',
+    desc: 'Another modal view',
+  },
+  {
+    img: './public/imgs/pokedex/modal_multi_2.png',
+    desc: 'Another modal evolution',
+  },
+  {
+    img: './public/imgs/pokedex/mobile.png',
+    desc: 'Mobile view',
+  },
+];
+
+const weather = [
+  {
+    img: './public/imgs/weather/main.png',
+    desc: 'Main page',
+  },
+  {
+    img: './public/imgs/weather/main_mobile.png',
+    desc: 'Main page mobile',
+  },
+];
+
+const imgCollection = {
+  anidisc,
+  dailyten,
+  gacha,
+  pokedex,
+  weather,
+};
+
+export default imgCollection;
